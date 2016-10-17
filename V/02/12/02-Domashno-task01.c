fa0dc9a7cc82f08b1b2b81b72420658ceda66d26
IvelinPetkov <ivelinv12@gmail.com>
2016-10-17 10:31:07 +0300

    Create 02-Domashno-task01.c

---
+++ V/02/12/02-Domashno-task01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b;
+printf("Enter a number from 1 to 9:");
+scanf("%d",&a);
+b=a;
+a=a*a;
+b=a*b;
+printf("Square:%d\n"
+       "Cube:%d",
+        a,b);
+
+}
