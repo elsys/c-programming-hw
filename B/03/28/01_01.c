d869da22f9dbd42f849419afa31e54791c483dae
thecatishere <thecatishere@thecatishere.thecatishere.org>
2016-10-19 22:53:49 +0300

    hw1 and hw2 commit

---
+++ B/03/28/01_01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a,b,i;
+    scanf("%d %d",&a,&b);
+    for(i = (a>b)? b:a;i > 0; i--) if (a%i == 0 && b%i == 0) break;
+    printf("%d",i);
+    return 0;
+}
