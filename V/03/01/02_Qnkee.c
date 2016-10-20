eb3643f657e04c46756390e5fc3fcc408a60d629
qnkee <gr0tato@gmail.com>
2016-10-20 04:01:59 +0300

    added 02

---
+++ V/03/01/02_Qnkee.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int num,b,c,j=0;
+	scanf("%d",&num);
+	c=0;
+	while(num>0){
+	c=num%10;
+	b=b+c;
+	num=num/10;
+	j++;
+	}
+	if(b>8){
+	printf("heavy");
+	}
+	else{
+	printf("light");
+}
+printf("%d",b);
+return 0;
+}
