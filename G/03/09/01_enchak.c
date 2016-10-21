1fcc68db821985b51fd2ded66a65622fd0d0ce64
Enchak <enchi.shahanov@gmail.com>
2016-10-21 20:06:51 +0300

    Create 01_enchak.c

---
+++ G/03/09/01_enchak.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int n,sum,a;
+ float avr;
+  	scanf("%d",&n);  
+ sum=0;
+for(int i=0;i<n;i++)
+{
+ scanf("%d",&a);  
+ sum=sum+a;
+}
+avr=0;
+avr=(float)sum/n;
+printf("Sum:%d\n",sum);
+printf("Avr:%.2f\n",avr);
+return 0;
+}
