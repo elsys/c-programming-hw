a39f2e8dbba40b02e316a0911e1be55bb16ece40
Lazar Frenkev <lazarfr2000@gmail.com>
2016-10-22 10:32:25 +0200

    Update 04_tapotodarvo.c

--- G/03/16/04_tapotodarvo.c
+++ G/03/16/04_tapotodarvo.c
@@ -3,8 +3,13 @@
 
 int main()
 {
-    int foot,count=1,i=0,brsp,brhash=1,control=0,control1=0,morest,safe=0,dodat,doonat;
+    int foot,count=1,i=0,brsp,brhash=1,control=0,control1=0,morest,safe=0,dodat,doonat,n=0;
     do{scanf("%d",&foot);}while(foot<3);
+    dodat=foot-2;
+    doonat=foot-3;
+    for(;dodat!=0;dodat--){i=i+n*2+1;n++;}
+    printf("%d\n",i);
+    i=0;
     dodat=foot-2;
     doonat=foot-3;
     do{
@@ -14,8 +19,10 @@ int main()
         }
         count++;
         if(control==0)printf("*");
-        if(control>0){
-            while(control1<dodat){
+        if(control>0)
+        {
+            while(control1<dodat)
+            {
                 printf("*");
                 safe=brhash;
                 do{
@@ -27,15 +34,15 @@ int main()
                 control1++;
                 break;
             }
-
         }else control++;
         i++;
-        if(i==foot){
+        if(i==foot)
+        {
             morest=5+doonat*2;
             for(;morest!=0;morest--)printf("*");
         }
         printf("\n");
-
     }while(i!=foot);
     return 0;
 }
+
