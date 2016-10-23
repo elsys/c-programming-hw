7b8d6865cd41980ecd833e19756ef4eb15c54342
Mario Iordanov <marioeqk@gmail.com>
2016-10-23 14:03:39 -0700

    Update 02.mariohw.c

--- G/03/18/02.mariohw.c
+++ G/03/18/02.mariohw.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
-int main()
-{
-int m,i,a,sum;
-float avr;
-scanf("%d",&m);
-sum=0;
-i=0;
-do{
-a=m%10;
-sum=sum+a;
-i++;
-m=m/10;
-}while(m>0);
-avr=sum/i;
-if(avr>7)
-{printf("\n heavy");}
-else
-{printf("\n light");}
-return 0;
-}
++#include<stdio.h>
+ int main()
+ {
+ int m,sum=0,i=0;
+ float avr;
+ scanf("%d",&m);
+ do
+ {
+ sum+=m%10;
+ i++;
+ m=m/10;
+ }while(m>0);
+ avr=sum/i;
+ if(avr>7){printf("\n heavy");}
+ else{printf("\n light");}
+ return 0;
+ }
