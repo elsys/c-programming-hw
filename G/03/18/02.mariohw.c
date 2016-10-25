ae1ea95ea75f70b67b0218d8d66e8faf5eb20ede
Mario Iordanov <marioeqk@gmail.com>
2016-10-25 09:53:07 -0700

    Update 02.mariohw.c

--- G/03/18/02.mariohw.c
+++ G/03/18/02.mariohw.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 int main()
-{
-int m,sum=0,i=0;
+{   
+int m,i;
 float avr;
-scanf("%d",&m);
-do
-{
-sum+=m%10;
+scanf("%d", &m);
+avr=0;
+i=0;
+do{
 i++;
+avr=avr+m%10;
 m=m/10;
 }while(m>0);
-avr=(float)sum/i;
-if(avr>7){printf("\n heavy");}
-else{printf("\n light");}
+avr=avr/i;
+if(avr>7){
+printf("heavy");
+}
+else{
+printf("light");
+}
 return 0;
 }
