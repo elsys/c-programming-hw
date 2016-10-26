ad421b53a0a223a254b2c551445cd9c9e2f3a21c
Blue devils <aleks.ton4eff@gmail.com>
2016-10-26 10:45:10 +0300

    Update 02_homework.c

--- G/03/02/02_homework.c
+++ G/03/02/02_homework.c
@@ -1,15 +1,23 @@
-  #include <stdio.h>
-  int main() {
-    int n,i,u;
-    float avg=0,sum=0;
-    scanf("%d", &n);
-    for(i=0;i<n;i++){
-      scanf("%d", &u);
-      sum=sum+u;
-      avg=avg+u;
+#include <stdio.h>
+
+int main()
+{   int a,i;
+    float s,b;
+    scanf("%d", &a);
+    s=0;
+    i=0;
+    do{
+        i++;
+        s=s+a%10;
+        a=a/10;
+        b=s;
+        b=b/i;
+ }while(a>0);
+    if(b<7){
+        printf("light\n");
     }
-    avg=avg/n;
-    printf("Sum: %.0f\n", sum);
-    printf("Avg: %.2f\n", avg);
+    else{
+        printf("heavy\n");
+    }
     return 0;
-  }
+}
