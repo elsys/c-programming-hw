a851dad8b2603df519da252ef08dc2dbe2d9114a
Blue devils <aleks.ton4eff@gmail.com>
2016-10-26 17:30:09 +0300

    Create 04_domashno.c

---
+++ 03/02/04_domashno.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stlib.h>
+int main (){
+    char c=' ', a= '*',b='#';
+    int i, z, y, u=0,f=1,d=1,p=1;
+    scanf("%d",&y);
+    for(;d<y-2;d++)
+        { p=p+2; f=f+p;}
+    printf("%d\n",f);
+    for(i=1; i<=y; ++i, u=0)
+    {
+        for(z=1; z<=y-i; ++z)
+        {
+            printf("%c",c);
+        }
+        while(u != 2*i-1)
+        { if (i==y) {printf ("%c",a); } else
+        if(u==0 || u==2*i-2) printf ("%c",a);else
+            { printf("%c",b);}
+                ++u;
+        }
+        printf("\n");
+        }
+        return 0;
+    }
