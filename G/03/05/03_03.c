5fd2a72eac6f19f45339297e46dcebff98293480
luci9 <luci9@users.noreply.github.com>
2016-10-27 23:45:08 +0300

    Update 03_03.c

--- G/03/05/03_03.c
+++ G/03/05/03_03.c
@@ -1,17 +1,19 @@
-int main() {
+#include <stdio.h>
+int main(){
     int n,br=0,i=0,j=0;
     float m,k,sum=0;
-scanf("%d", &n);
-scanf("%f", &m);
-scanf("%f", &k);
+    scanf("%d", &n);
+    scanf("%f", &m);
+    scanf("%f", &k);
     do{
         do{
         sum=sum+m*(100-j)/100;
-        i++;}while(i<k);
-        j=j+2;
-        br=br+i+1;
-        i=0;
-        }while(br<=n);
-printf("f", sum);
+        i++;
+        }while(i<k);
+    j=j+2;
+    br=br+i+1;
+    i=0;
+    }while(br<=n);
+    printf("%.0f", sum);
     return 0;
 }
