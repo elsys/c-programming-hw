a779e9f83b7099ba7ef2558c26ce6a0218c02a5a
Trayan1 <trayan333@gmail.com>
2016-11-13 13:35:28 +0200

    homework

--- G/04/25/03_task03.c
+++ G/04/25/03_task03.c
@@ -2,17 +2,4 @@
 
 int main()
 {
-   char str[1000], ch;
-   int i, frequency = 0;
-   getchar();
- 
-   for(i = 0; str[i] != '\0'; ++i)
-   {
-       if(ch == str[i])
-           ++frequency;
-   }
-
-   printf(" %c - %d", ch, frequency);
-
-   return 0;
 }
--- G/04/25/04_task04.c
+++ G/04/25/04_task04.c
@@ -1,8 +1,5 @@
 #include<stdio.h>
 int main()
 {
-int i;
-float a[64];
-
 
 }
