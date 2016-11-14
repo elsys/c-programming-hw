2fe295929179f271767a33d26985bb6b955bd489
Digitalc0m <mihailovkonstantin364@gmail.com>
2016-11-14 21:07:51 +0200

    update

--- B/04/13/task02.c
+++ B/04/13/task02.c
@@ -2,23 +2,26 @@
 
 int main()
 {
-int n,o=0,i=1;
-
-int read[100];
+int n,o;
+int i=0;
 int odds[100];
 
 scanf("%d",&n);
 
 do{
-
-scanf("%d",&read[o]);
-if(read[o]%2!=0){odds[n-i]=read[o];i++;}o++;
-}while(i<=n);
-o=1;
+scanf("%d",o);
+   if(o%2!=0){
+     odds[i]=o;
+     i++;
+   }
+}while(i<n);
+   
+i=0;
+   
 do{
-printf("%d\n",odds[n-o]-odds[o-1]);
-o++;
-}while(n/2>o-1);
+ printf("%d\n",odds[i]-odds[n-i-1]);
+i++;
+}while(n/2>i);
 
 if(n%2!=0)printf("%d\n",odds[n/2]);
    
