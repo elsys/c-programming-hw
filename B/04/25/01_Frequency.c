ace9ae5084bb3c6f0ea30cec075701aafab884e3
Спас Зидаров <zidarovspas@gmail.com>
2016-11-19 01:14:08 +0200

    Update 01_Frequency.c

--- B/04/25/01_Frequency.c
+++ B/04/25/01_Frequency.c
@@ -1,17 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int n, a=0;
-	scanf("%d", &n);
-	int b[n];
-	for(;a<n;a++)
-		{
-			scanf("%d", &b[a]);
-		}
-	for(a--;a>=0;a--)
-	{
-		printf("%d\n", b[a]);
-	}
-return 0;
+        int n,i,arr[n];
+        scanf("%d", &n);
+        for(i = 0; i < n; i++){
+                scanf("%d", &arr[i]);
+        }
+        for(i = n - 1; i >= 0; i--){
+                printf("%d\n", arr[i]);
+        }
+        return 0;
 }
