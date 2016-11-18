394eaaa7843d5386f1d956bb64a280165c78b04f
boguna <bogipetrov@gmail.com>
2016-11-18 11:35:54 +0200

    domashno

---
+++ G/04/04/01_dom01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int broi,br;
+int mas[broi];
+do
+{scanf("%d", &broi);
+}while(br>0);
+for(br=1;br<=broi;br++)
+{
+scanf("%d", &mas[br]);
+}
+for(br=broi;br>0;br--)
+{
+printf("%d\n", mas[br]);
+}
+return 0;
+}
+
+
+
