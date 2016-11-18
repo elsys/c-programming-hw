48c48e340b0c2c19febce2a0d887e080badbad16
Спас Зидаров <zidarovspas@gmail.com>
2016-11-19 00:43:44 +0200

    Update 01_Frequency.c

--- B/04/25/01_Frequency.c
+++ B/04/25/01_Frequency.c
@@ -1,34 +1,17 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 
 int main()
 {
-	char snts[1000];
-	int alph[26]={0},i=0;	
-	fgets(snts,1000,stdin);
-	while(snts[i]!='\0') 
-	{
-	        if(snts[i]>='A'&&snts[i]<='Z') 
+	int n, a=0;
+	scanf("%d", &n);
+	int b[n];
+	for(;a<n;a++)
 		{
-        		snts[i]=snts[i]+32;
-        	}
-       		i++;
+			scanf("%d", &b[a]);
+		}
+	for(a--;a>=0;a--)
+	{
+		printf("%d\n", b[a]);
 	}
-	i=0;
-	while(snts[i]!='\0')
-	{
-	        if(snts[i]>='a'&&snts[i]<='z')
-		{
-	            alph[snts[i]-'a']++;
-	        }
-        i++;
-	}
-	for(i=0;i<26;i++)
-	{
-        if(alph[i]!=0)
-	{
-            printf("%c - %d\n",i+'a',alph[i]);
-        }
-    }
-    return 0;
+return 0;
 }
