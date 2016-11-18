694d04afd655a3b691860a340c500f27a8813dc3
Tony Kwon <Merlinaz@Merlinaz>
2016-11-18 22:06:49 +0200

    Pls imam dr i po Bashewa

--- V/04/26/04_Tony.c
+++ V/04/26/04_Tony.c
@@ -4,34 +4,28 @@ int main()
     int a=0;
     float arr[64];
     float sumof=0;
-    while(scanf("%f",&arr[a])!=EOF)
+    while (scanf("%f",&arr[a])!=EOF)
 	{
         int b=1;
-        for(int j=1;j<a;j++)
-	{
-            if(arr[j]==arr[a])
-		{
-               		 b=0;
-            	}
+        for(int j=0; j<a; j++){
+            if(arr[j]==arr[a]){
+                b=0;
+            }
         }
-        if(b)
-	{
+        
+        if(b){
             a++;
         }
-    	}
-    		for(int k=1;k<a;k++)
-		{
-       		 float n=arr[k];
-       		 if(n>=a)
-		{
-        	  do
-		{
+    }
+    for(int k=0; k<a; k++){
+        float n=arr[k];
+        if(n>=a){
+            do{
                 n=n-a;
-            	}while(n>=a);
-        	}
-		sumof=sumof+n;
-    		}
-	    printf("%d",a);
-	    printf("\n%.3f",sumof);
-	return 0;
+            }while(n>=a);
+        }
+        sumof=sumof+n;
+    }
+    printf("%d", a);
+    printf("\n%.3f", sumof);
 }
