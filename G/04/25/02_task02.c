0de82eb23fc8209043ba7230f6eb80c8228f2cda
Trayan1 <trayan333@gmail.com>
2016-11-18 20:36:20 +0200

    homework

--- G/04/25/02_task02.c
+++ G/04/25/02_task02.c
@@ -7,7 +7,7 @@ int a[n];
 for(i=0;i<n;i++)
 {do{scanf("%d",&a[i]);}while(a[i]%2==0);}
 r=n-1;
-if(n%2!=0){ printf("\n %d",a[r/2]);}
+if(n%2!=0){ printf("\n %d",a[n/2]);}
 else{for(i=0;i<(n-1)/2;i++)
 {b=a[i]-a[r];
 printf("%d\n",b);
