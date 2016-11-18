19f9ee8da9449ba9dd0e218f361f39874ce2eaf0
Спас Зидаров <zidarovspas@gmail.com>
2016-11-19 01:18:10 +0200

    Update 01_Frequency.c

--- B/04/25/01_Frequency.c
+++ B/04/25/01_Frequency.c
@@ -1,33 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int n, brr=0;
+	int n,i,arr[n];
 	scanf("%d", &n);
-	int arr[n], sum=0;
-	while(brr<n)
-	{
-		scanf("%d", &arr[brr]);
-		if(arr[brr]%2!=0)
-		brr++;
+	for(i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
 	}
-	if(n%2==0)
-	{
-		for(brr=0;brr<n/2;brr++)
-		{
-			sum=arr[brr]-arr[(n-1)-brr];
-			printf("%d\n", sum);
-		}
-	}
-	else
-	{
-		n--;
-		for(brr=0;brr<n/2;brr++)
-		{
-			sum=arr[brr]-arr[n-brr];
-			printf("%d\n", sum);
-		}
-		printf("%d\n", arr[brr]);
+	for(i = n - 1; i >= 0; i--){
+		printf("%d\n", arr[i]);
 	}
 	return 0;
 }
