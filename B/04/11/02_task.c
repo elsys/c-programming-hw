0161c9254cb34fc7e39ab70d54f53e8d3975c4d1
Iliyan <iliqn.tachev@gmail.com>
2016-11-19 01:49:10 +0200

    2 fixed time

--- B/04/11/02_task.c
+++ B/04/11/02_task.c
@@ -1,25 +1,20 @@
 #include<stdio.h>
 int main()
 {
- int n,k=0,k2=0,i=0,masiv[100],masiv2[100];
+ int n=0,i,k=0,masiv[100],chislo;
  scanf("%d",&n);
-   while(k<=i)
+   while(k<n)
    {
-     scanf("%d",&masiv[i]);
-     if(masiv[i]%2!=0){masiv2[k]=masiv[i];k++;}
-     if(k==n)break;
-     i++;
-     k2=k;
+     scanf("%d",&chislo);
+     if(chislo%2!=0){masiv[k]=chislo;k++;}
    }
    i=0;
-   for(;i<(k/2);)
+   for(;i<(n/2);)
    { 
-      printf("%d\n",masiv2[i]-masiv2[k2]);
-      i++;
-      k2--;
-      if(n%2!=0)printf("%d",masiv2[k/2]);
-    
+      printf("%d\n",(masiv[i]-masiv[(n-1)-i]));
+     i++;
    }
+if(n%2!=0)printf("%d",masiv[n/2]);
  
 return 0;
 }
