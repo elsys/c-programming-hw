b450772207953b2ce20c83b30e15cfe215edfa80
Simonsi <simona.vangelova23@gmail.com>
2016-11-18 23:59:31 +0200

    Update 04_task.c

--- V/04/23/04_task.c
+++ V/04/23/04_task.c
@@ -6,23 +6,23 @@ int main()
     int i,x,j,n;
     i=0;
     n=0;
-    float arr[1000],num,sum=0;
-    while((scanf("%f",&num))!=EOF)
+    float a[1000],num,sum=0;
+    whie((scanf("%f",&num))!=EOF)
         {
             x=0;
             for(j=0;j<64;j++){
 
-            if(arr[j]==num) x=1;
+            if(a[j]==num) x=1;
             if(x==0)
             {
-            arr[i]=num;
+            a[i]=num;
             i++;
             }
             }
     }
     while(n < i)
     {
-        sum += fmod(arr[n], i);
+        sum += fmod(a[n], i);
         n++;
     }
 	printf("%d\n%.3f ",i,sum);
