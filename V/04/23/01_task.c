6e0ec4087f2a06087712172e4c9acf5348ff5ce8
Simonsi <simona.vangelova23@gmail.com>
2016-11-18 15:37:56 +0200

    Update 01_task.c

--- V/04/23/01_task.c
+++ V/04/23/01_task.c
@@ -3,22 +3,20 @@
 int main()
 {
 	int numCount = 0;
-    int i=0;
+    	int i=0;
 	int arr[1024];
 
 	while ((numCount <= 0) || (numCount > 1024))
 	{
-		printf("Vavedete broi chisla\n");
 		scanf("%d", &numCount);
 	}
 
-	printf("Zapochnete vuvezhdane na chisla\n");
 	for (i = 0; i < numCount; i++)
 	{
 		scanf("%d", &(arr[i]));
 	}
 
-	printf("Chislata v obraten red sa:\n");
+
 	for (i = numCount - 1; i >= 0; i--)
 	{
 		printf("%d\n", arr[i]);
